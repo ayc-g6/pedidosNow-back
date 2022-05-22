1e765c4043e06ab7a8deb47cc608b7991498fbe8
German-Rotili <german.rotili@gmail.com>
2022-05-22 18:17:13 -0300

    Agrego bugfix para heroku postgres default env

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-POSTGRE_SQL_URL = os.getenv('DATABASE_URL')
+POSTGRE_SQL_URL = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1) 
 print(POSTGRE_SQL_URL)
 engine = create_engine(POSTGRE_SQL_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
